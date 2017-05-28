041fd24b529ffd30f41af95951ef953ec0747733
victor <vpalmacarrasco@gmail.com>
2017-05-28 18:04:13 +0200

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/android/deezfoodz/dagger/AppComponent.java
@@ -0,0 +1,23 @@
+package com.raywenderlich.android.deezfoodz.dagger;
+
+import com.raywenderlich.android.deezfoodz.ui.food.FoodActivity;
+import com.raywenderlich.android.deezfoodz.ui.food.FoodPresenterImpl;
+import com.raywenderlich.android.deezfoodz.ui.foodz.FoodzActivity;
+import com.raywenderlich.android.deezfoodz.ui.foodz.FoodzPresenterImpl;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by victor on 21/5/17.
+ */
+
+@Singleton
+@Component(modules = {AppModule.class, PresenterModule.class, NetworkModule.class})
+public interface AppComponent {
+    void inject(FoodzActivity target);
+    void inject(FoodActivity target);
+    void inject(FoodzPresenterImpl target);
+    void inject(FoodPresenterImpl target);
+}
